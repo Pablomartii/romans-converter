b5bd604ac3416e8626afa9ae1e9dc20569faa6a6
Pablomartii <pabloms1007@gmail.com>
2025-05-16 10:09:23 +0200

    Update converter.js

--- converter.js
+++ converter.js
@@ -41,22 +41,20 @@ function romanToInteger(roman) {
   }
 
   roman = roman.toUpperCase();
+
   if (!/^[IVXLCDM]+$/.test(roman)) {
     throw new Error('The Roman numeral contains invalid characters.');
   }
 
   const romanMap = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
+    'I': 1, 'V': 5, 'X': 10,
+    'L': 50, 'C': 100,
+    'D': 500, 'M': 1000
   };
 
   let total = 0;
   let previousValue = 0;
+
   for (let i = roman.length - 1; i >= 0; i--) {
     const currentValue = romanMap[roman[i]];
     if (currentValue < previousValue) {
